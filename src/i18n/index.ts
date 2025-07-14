0524b5645cd0657bfbe28ae86ad869bf05f2417c
Okan Ay <okanay@hotmail.com>
2025-07-14 18:04:26 +0300

    Not Found Error Message

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -13,37 +13,25 @@ import resource, { defaultNS, ns } from 'src/messages'
 
 const i18nConfig = (initialLanguage: LanguageValue = DEFAULT_LANGUAGE.value) => {
   if (!i18n.isInitialized) {
-    i18n
-      .use(Backend)
-      .use(initReactI18next)
-      .init({
-        lng: initialLanguage,
-        defaultNS: defaultNS,
-        ns: ns,
-        // resources: resource,
-        backend: {
-          loadPath: 'https://assets.hoi.com.tr/messages/{{lng}}/{{ns}}.json',
-          crossDomain: true,
-          requestOptions: {
-            cache: 'default',
-            expirationTime: 7 * 24 * 60 * 60 * 1000,
-          },
-        },
-        fallbackLng: FALLBACK_LANGUAGE.value,
-        supportedLngs: LANGUAGES_VALUES,
-
-        interpolation: {
-          escapeValue: false,
-        },
-        react: {
-          useSuspense: true,
-        },
-        detection: {
-          lookupCookie: I18N_COOKIE_NAME,
-          lookupLocalStorage: I18N_STORAGE_KEY,
-          caches: ['localStorage', 'cookie'],
-        },
-      })
+    i18n.use(initReactI18next).init({
+      lng: initialLanguage,
+      defaultNS: defaultNS,
+      ns: ns,
+      resources: resource,
+      fallbackLng: FALLBACK_LANGUAGE.value,
+      supportedLngs: LANGUAGES_VALUES,
+      interpolation: {
+        escapeValue: false,
+      },
+      react: {
+        useSuspense: true,
+      },
+      detection: {
+        lookupCookie: I18N_COOKIE_NAME,
+        lookupLocalStorage: I18N_STORAGE_KEY,
+        caches: ['localStorage', 'cookie'],
+      },
+    })
   } else if (i18n.language !== initialLanguage) {
     i18n.changeLanguage(initialLanguage)
   }
